b4844bca98e8f6b7167dbdbd8097f9fc6de10e17
Frankie <orangeprince@hushmail.com>
2014-03-21 02:53:23 +0100

    rails new demo-mangler

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoMangler::Application.config.secret_key_base = 'dfacaf15ce5dd62f38f17ead8845e1607640fc8b2add09f7a3ee86978ff85c4e3bc190bd0c4234107263289ce5913edf84810c25e681d1e76e47614cb37cdc35'
